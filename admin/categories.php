c3e3ef34f3a0d0c95ca0eff45f3a31d6eb5c1b56
Mae <perez.066155@munoz.sti.edu.ph>
2020-02-12 01:55:14 +0800

    refactoring the code

--- admin/categories.php
+++ admin/categories.php
@@ -1,104 +1,105 @@
-<?php  include "includes/admin_header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
 
-    <div id="wrapper">
-
-
+<div id="wrapper">
 
 
-        <!-- Navigation -->
-     <?php  include "includes/admin_navigation.php"   ?>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
 
 
-                        <h1 class="page-header">
-                            Welcome to Admin
-                            <small>Mae</small>
-                        </h1>
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"   ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
 
 
-              
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Mae</small>
+                    </h1>
+
+
+
 
                     <div class="col-xs-6">
 
-                    <?php  insert_categories(); ?>
-  
-                    <form action="" method="post">
-                  
-                    <div class="form-group">
-                            <label for="cat-title">Add Category</label>
-                            <input type="text" class="form-control" name="cat_title" >
-                        </div>
-                        <div class="form-group">
-                            <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-                        </div>
+                        <?php insert_categories(); ?>
 
-                    </form>
-                    
-                    
-                    <?php
+                        <form action="" method="post">
 
-                    //UPDATE and include QUERY
+                            <div class="form-group">
+                                <label for="cat-title">Add Category</label>
+                                <input type="text" class="form-control" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
 
-                    if(isset($_GET['edit'])){
-
-                        $cat_id = $_GET['edit'];
-
-                        include "includes/update_categories.php";
-                    }
-
-                    ?>
+                        </form>
 
 
-                    </div><!-----Add Category Form--->
+                        <?php
+
+                        //UPDATE and include QUERY
+
+                        if (isset($_GET['edit'])) {
+
+                            $cat_id = $_GET['edit'];
+
+                            include "includes/update_categories.php";
+                        }
+
+                        ?>
+
+
+                    </div>
+                    <!-----Add Category Form--->
 
 
                     <div class="col-xs-6">
 
                         <table class="table table-bordered table-hover">
-                          <thead>
-                              <tr>
-                                  <th>ID</th>
-                                  <th>Category Title</th>
-                              </tr>
-                          </thead>
-                          <tbody>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                        
-                        
-                         <?php findAllCategories(); ?>
-                     
 
-                        <?php deleteCategories();  ?>
 
-                        
-                             
-                          </tbody>
-                      </table>
-                    
+                                <?php findAllCategories(); ?>
+
+
+                                <?php deleteCategories();  ?>
+
+
+
+                            </tbody>
+                        </table>
+
                     </div>
 
 
-                       
-                    </div>
+
                 </div>
-                <!-- /.row -->
-
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
+        <!-- /.container-fluid -->
+
+    </div>
 
 
 
-        <!-- /#page-wrapper -->
+    <!-- /#page-wrapper -->
 
 
 
-<?php include "includes/admin_footer.php"; ?>
+    <?php include "includes/admin_footer.php"; ?>
