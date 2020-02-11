4f139bfe825cbd65094734c8fddd2d4fb62910fa
Mae <perez.066155@munoz.sti.edu.ph>
2020-02-12 00:20:55 +0800

    Add functions to update category

--- admin/categories.php
+++ admin/categories.php
@@ -65,44 +65,16 @@
                     </form>
                     
                     
-                    
-                    <form action="" method="post">
-                  
-                    <div class="form-group">
-                            <label for="cat-title">Edit Category</label>
+                    <?php
 
-                            <?php
+                    if(isset($_GET['edit'])){
 
-                            if(isset($_GET['edit'])){
+                        $cat_id = $_GET['edit'];
 
-                                $cat_id = $_GET['edit'];
-                                $query="SELECT * FROM categories WHERE cat_id = $cat_id";
-                                $select_categories_id=mysqli_query($connection,$query);
-        
-        
-                                while($row=mysqli_fetch_assoc($select_categories_id)){
-                                $cat_id=$row['cat_id'];
-                                $cat_title=$row['cat_title'];
-                                
-                                ?>
+                        include "includes/update_categories.php";
+                    }
 
-                                
-                            <input  value="<?php if(isset($cat_title)){ echo $cat_title; } ?>" type="text" class="form-control" name="cat_title" >
-    
-                             <?php   } }  ?>                        
-
-                             <?php
-
-
-                            ?>
-                            
-                        </div>
-                        <div class="form-group">
-                            <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
-                        </div>
-
-                    </form>
-
+                    ?>
 
 
                     </div><!-----Add Category Form--->
