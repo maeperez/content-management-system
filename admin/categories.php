d747376808c83da69d166ce05fdb1328c106b9c0
Mae <perez.066155@munoz.sti.edu.ph>
2020-02-11 14:39:06 +0800

    Adding Function For deleting Category

--- admin/categories.php
+++ admin/categories.php
@@ -68,13 +68,7 @@
 
                     <div class="col-xs-6">
 
-                    <?php 
-
-                    $query="SELECT * FROM categories";
-                    $select_categories=mysqli_query($connection,$query);
-
-                    ?>
-                      <table class="table table-bordered table-hover">
+                        <table class="table table-bordered table-hover">
                           <thead>
                               <tr>
                                   <th>ID</th>
@@ -83,8 +77,12 @@
                           </thead>
                           <tbody>
 
-                        <?php      
-
+                        <?php //Find All Categories
+                        
+                        $query="SELECT * FROM categories";
+                        $select_categories=mysqli_query($connection,$query);
+    
+    
                         while($row=mysqli_fetch_assoc($select_categories)){
                             $cat_id=$row['cat_id'];
                             $cat_title=$row['cat_title'];
@@ -92,10 +90,23 @@
                         
                         echo "<tr>";
                         echo "<td>{$cat_id}</td>";
-                        echo "<td>{$cat_title}</td>";              
+                        echo "<td>{$cat_title}</td>";  
+                        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";                 
                         echo "</tr>";
                       
                         }
+
+                        ?>
+
+                        <?php
+
+                            if(isset($_GET['delete'])){
+                            $the_cat_id = $_GET['delete'];
+
+                            $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                            $delete_query=mysqli_query($connection, $query);
+                            header("Location: categories.php");    
+                            }
 
                         ?>
                              
